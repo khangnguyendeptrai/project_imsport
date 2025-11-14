ab3326466ae0d4e8092c873142f774f30f516630
DoNguyenTai <donguyentai05@gmail.com>
2025-11-15 00:05:37 +0700

    update

---
+++ src/data/ProductVariation.js
@@ -0,0 +1,264 @@
+const productsData = [
+    {
+        "id": 1,
+        "category_id": 1,
+        "name": "Áo Chạy Địa Hình Nam Raidlight Maillot de trail R-Light - KAKI",
+        "image": "https://pos.nvncdn.com/be3294-43017/ps/20250609_rmhy0z22Wk.jpeg",
+        "thumbnail": [
+          "https://pos.nvncdn.com/be3294-43017/ps/20250609_rmhy0z22Wk.jpeg?v=1749535086",
+          "https://pos.nvncdn.com/be3294-43017/ps/20250609_tZaYYxHTW1.jpeg?v=1749466078",
+          "https://pos.nvncdn.com/be3294-43017/ps/20250609_wfv0nzo5lG.webp?v=1749466079",
+          "https://pos.nvncdn.com/be3294-43017/ps/20250609_Zm6rxfHwtN.webp?v=1749466080"
+        ],
+        "price": 1850000,
+        "brand": "Raidlight",
+        "size": null,
+        "originalPrice": 0,
+        "isBestSeller": false,
+        "isDiscount": 0,
+        "isGift": false,
+        description: `
+              <div class="p-0"> 
+                <h3 class="text-xl font-bold mb-3">Áo chạy bộ On Pace Mesh-T</h3>
+                <p class="mb-8">
+                Hiệu suất cao – Siêu nhẹ – Thoáng khí tối đa
+                <br/>
+                Được thiết kế dành riêng cho các vận động viên chạy bộ, Pace Mesh-T kết hợp giữa hiệu suất vượt trội và sự thoải mái tối đa.
+                </p>
+                <div class="mb-8">
+                  <img src="https://pos.nvncdn.com/be3294-43017/ps/20250609_rmhy0z22Wk.jpeg" alt="Hình ảnh chi tiết sản phẩm" class="w-full h-auto object-cover mb-3"/>
+                </div>
+                <h4 class="font-bold mt-4 mb-2">Tính năng nổi bật</h4>
+                <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+                  <li>Chất liệu CleanCloud® Polyester: Sử dụng sợi polyester tái chế từ khí thải carbon, mang lại cảm giác mềm mại và co giãn tuyệt vời.</li>
+                  <li>Siêu nhẹ và thoáng khí: Thiết kế giúp duy trì sự mát mẻ và khô ráo trong suốt quá trình chạy.</li>
+                  <li>Co giãn linh hoạt và mềm mại: Tạo cảm giác thoải mái tối đa, cho phép chuyển động tự nhiên mà không bị hạn chế.</li>
+                  <li>Logo phản quang của On: Tăng khả năng nhận diện trong điều kiện ánh sáng yếu, đảm bảo an toàn khi chạy vào sáng sớm hoặc chiều tối.</li>
+                    <li>Chi tiết đồ họa lấy cảm hứng từ văn hóa chạy bộ: Mang đến phong cách năng động và hiện đại.</li>
+                      <li>Đường viền không may (bonded hems): Giảm thiểu ma sát, tăng cường sự thoải mái và độ bền cho sản phẩm.</li>
+                        <li>Mắt cài số bib tích hợp: Giúp giữ số bib chắc chắn mà không làm hỏng áo.</li>
+                </ul>
+                <h4 class="font-bold mt-4 mb-2">Phom dáng & chất liệu</h4>
+                <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+                  <li>Phom dáng: Regular fit – vừa vặn với cơ thể, không quá ôm sát.</li>
+                  <li>Chất liệu chính: 81% Polyester, 19% Elastane.</li>
+                  <li>Viền cổ: 97% Polyester tái chế, 3% Elastane. </li>
+                </ul>
+                <h4 class="font-bold mt-4 mb-2">Hướng dẫn bảo quản</h4>
+                <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+                  <li>Giặt tay hoặc máy ở chế độ lạnh và nhẹ nhàng</li>
+                  <li>Không dùng thuốc tẩy</li>
+                  <li>Không giặt khô</li>
+                  <li>Không ủi (là)</li>
+                  <li>Có thể sấy khô ở nhiệt độ thấp</li>
+                  <li>Giặt mặt trái áo.</li>
+                      <li>Giặt riêng với đồ khác</li>
+                      Pace Mesh-T là lựa chọn hoàn hảo cho những runner đề cao cảm giác nhẹ, thoáng và hiệu suất. Dù là chạy luyện tập hay ngày thi đấu, chiếc áo này sẽ giúp bạn di chuyển tự nhiên và tự tin hơn mỗi km.
+                </ul>
+                <h4 class="font-bold mt-6 mb-3 ">Bảng size tham khảo</h4>
+                <img src="https://pos.nvncdn.com/be3294-43017/ps/20251018_9qgbuRDFZC.jpeg" alt="Bảng size giày" class="w-full h-auto object-cover mb-3"/>
+              </div>
+            `,
+      },
+      {
+        "id": 2,
+        "category_id": 1,
+        "name": "Áo Khoác Gió Nam NNormal Trail Wind Jacket - Light Green",
+        "image": "https://pos.nvncdn.com/be3294-43017/ps/20251018_9qgbuRDFZC.jpeg",
+        "thumbnail": [
+          "https://pos.nvncdn.com/be3294-43017/ps/20251018_9qgbuRDFZC.jpeg?v=1761295804",
+          "https://pos.nvncdn.com/be3294-43017/ps/20251018_9HF5aAnmFy.png?v=1760756571",
+          "https://pos.nvncdn.com/be3294-43017/ps/20251018_5RKtpHQytY.png?v=1760756573",
+          "https://pos.nvncdn.com/be3294-43017/ps/20251018_m8WIImzMDo.png?v=1760756576"
+        ],
+        "price": 4999000,
+        "brand": "NNormal",
+        "size": null,
+        "originalPrice": 0,
+        "isBestSeller": false,
+        "isDiscount": 0,
+        "isGift": false,
+        "description": `
+              <div class="p-0"> 
+                <h3 class="text-xl font-bold mb-3">Áo chạy bộ On Pace Mesh-T</h3>
+                <p class="mb-8">
+                Hiệu suất cao – Siêu nhẹ – Thoáng khí tối đa
+                <br/>
+                Được thiết kế dành riêng cho các vận động viên chạy bộ, Pace Mesh-T kết hợp giữa hiệu suất vượt trội và sự thoải mái tối đa.
+                </p>
+                <div class="mb-8">
+                  <img src="https://pos.nvncdn.com/be3294-43017/ps/20251018_9qgbuRDFZC.jpeg" alt="Hình ảnh chi tiết sản phẩm" class="w-full h-auto object-cover mb-3"/>
+                </div>
+                <h4 class="font-bold mt-4 mb-2">Tính năng nổi bật</h4>
+                <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+                  <li>Chất liệu CleanCloud® Polyester: Sử dụng sợi polyester tái chế từ khí thải carbon, mang lại cảm giác mềm mại và co giãn tuyệt vời.</li>
+                  <li>Siêu nhẹ và thoáng khí: Thiết kế giúp duy trì sự mát mẻ và khô ráo trong suốt quá trình chạy.</li>
+                  <li>Co giãn linh hoạt và mềm mại: Tạo cảm giác thoải mái tối đa, cho phép chuyển động tự nhiên mà không bị hạn chế.</li>
+                  <li>Logo phản quang của On: Tăng khả năng nhận diện trong điều kiện ánh sáng yếu, đảm bảo an toàn khi chạy vào sáng sớm hoặc chiều tối.</li>
+                    <li>Chi tiết đồ họa lấy cảm hứng từ văn hóa chạy bộ: Mang đến phong cách năng động và hiện đại.</li>
+                      <li>Đường viền không may (bonded hems): Giảm thiểu ma sát, tăng cường sự thoải mái và độ bền cho sản phẩm.</li>
+                        <li>Mắt cài số bib tích hợp: Giúp giữ số bib chắc chắn mà không làm hỏng áo.</li>
+                </ul>
+                <h4 class="font-bold mt-4 mb-2">Phom dáng & chất liệu</h4>
+                <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+                  <li>Phom dáng: Regular fit – vừa vặn với cơ thể, không quá ôm sát.</li>
+                  <li>Chất liệu chính: 81% Polyester, 19% Elastane.</li>
+                  <li>Viền cổ: 97% Polyester tái chế, 3% Elastane. </li>
+                </ul>
+                <h4 class="font-bold mt-4 mb-2">Hướng dẫn bảo quản</h4>
+                <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+                  <li>Giặt tay hoặc máy ở chế độ lạnh và nhẹ nhàng</li>
+                  <li>Không dùng thuốc tẩy</li>
+                  <li>Không giặt khô</li>
+                  <li>Không ủi (là)</li>
+                  <li>Có thể sấy khô ở nhiệt độ thấp</li>
+                  <li>Giặt mặt trái áo.</li>
+                      <li>Giặt riêng với đồ khác</li>
+                      Pace Mesh-T là lựa chọn hoàn hảo cho những runner đề cao cảm giác nhẹ, thoáng và hiệu suất. Dù là chạy luyện tập hay ngày thi đấu, chiếc áo này sẽ giúp bạn di chuyển tự nhiên và tự tin hơn mỗi km.
+                </ul>
+                <h4 class="font-bold mt-6 mb-3 ">Bảng size tham khảo</h4>
+                <img src="https://pos.nvncdn.com/be3294-43017/ps/20251018_9qgbuRDFZC.jpeg" alt="Bảng size giày" class="w-full h-auto object-cover mb-3"/>
+              </div>
+            `,
+      },
+      {
+        "id": 3,
+        "category_id": 1,
+        "name": "Áo Khoác Gió Nam NNormal Trail Wind Jacket - White",
+        "image": "https://pos.nvncdn.com/be3294-43017/ps/20251018_l4UTnFHDzR.jpeg",
+        "thumbnail": [
+          "https://pos.nvncdn.com/be3294-43017/ps/20251018_9qgbuRDFZC.jpeg?v=1761295804",
+          "https://pos.nvncdn.com/be3294-43017/ps/20251018_9HF5aAnmFy.png?v=1760756571",
+          "https://pos.nvncdn.com/be3294-43017/ps/20251018_5RKtpHQytY.png?v=1760756573",
+          "https://pos.nvncdn.com/be3294-43017/ps/20251018_m8WIImzMDo.png?v=1760756576"
+        ],
+        "price": 4999000,
+        "brand": "NNormal",
+        "size": null,
+        "originalPrice": 0,
+        "isBestSeller": false,
+        "isDiscount": 0,
+        "isGift": false,
+        description: `
+        <div class="p-0"> 
+          <h3 class="text-xl font-bold mb-3">Áo chạy bộ On Pace Mesh-T</h3>
+          <p class="mb-8">
+          Hiệu suất cao – Siêu nhẹ – Thoáng khí tối đa
+          <br/>
+          Được thiết kế dành riêng cho các vận động viên chạy bộ, Pace Mesh-T kết hợp giữa hiệu suất vượt trội và sự thoải mái tối đa.
+          </p>
+          <div class="mb-8">
+            <img src="https://pos.nvncdn.com/be3294-43017/ps/20251018_l4UTnFHDzR.jpeg" alt="Hình ảnh chi tiết sản phẩm" class="w-full h-auto object-cover mb-3"/>
+          </div>
+          <h4 class="font-bold mt-4 mb-2">Tính năng nổi bật</h4>
+          <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+            <li>Chất liệu CleanCloud® Polyester: Sử dụng sợi polyester tái chế từ khí thải carbon, mang lại cảm giác mềm mại và co giãn tuyệt vời.</li>
+            <li>Siêu nhẹ và thoáng khí: Thiết kế giúp duy trì sự mát mẻ và khô ráo trong suốt quá trình chạy.</li>
+            <li>Co giãn linh hoạt và mềm mại: Tạo cảm giác thoải mái tối đa, cho phép chuyển động tự nhiên mà không bị hạn chế.</li>
+            <li>Logo phản quang của On: Tăng khả năng nhận diện trong điều kiện ánh sáng yếu, đảm bảo an toàn khi chạy vào sáng sớm hoặc chiều tối.</li>
+              <li>Chi tiết đồ họa lấy cảm hứng từ văn hóa chạy bộ: Mang đến phong cách năng động và hiện đại.</li>
+                <li>Đường viền không may (bonded hems): Giảm thiểu ma sát, tăng cường sự thoải mái và độ bền cho sản phẩm.</li>
+                  <li>Mắt cài số bib tích hợp: Giúp giữ số bib chắc chắn mà không làm hỏng áo.</li>
+          </ul>
+          <h4 class="font-bold mt-4 mb-2">Phom dáng & chất liệu</h4>
+          <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+            <li>Phom dáng: Regular fit – vừa vặn với cơ thể, không quá ôm sát.</li>
+            <li>Chất liệu chính: 81% Polyester, 19% Elastane.</li>
+            <li>Viền cổ: 97% Polyester tái chế, 3% Elastane. </li>
+          </ul>
+          <h4 class="font-bold mt-4 mb-2">Hướng dẫn bảo quản</h4>
+          <ul class="list-disc list-inside ml-4 mb-4 space-y-1">
+            <li>Giặt tay hoặc máy ở chế độ lạnh và nhẹ nhàng</li>
+            <li>Không dùng thuốc tẩy</li>
+            <li>Không giặt khô</li>
+            <li>Không ủi (là)</li>
+            <li>Có thể sấy khô ở nhiệt độ thấp</li>
+            <li>Giặt mặt trái áo.</li>
+                <li>Giặt riêng với đồ khác</li>
+                Pace Mesh-T là lựa chọn hoàn hảo cho những runner đề cao cảm giác nhẹ, thoáng và hiệu suất. Dù là chạy luyện tập hay ngày thi đấu, chiếc áo này sẽ giúp bạn di chuyển tự nhiên và tự tin hơn mỗi km.
+          </ul>
+          <h4 class="font-bold mt-6 mb-3 ">Bảng size tham khảo</h4>
+          <img src="https://pos.nvncdn.com/be3294-43017/ps/20251018_9qgbuRDFZC.jpeg" alt="Bảng size giày" class="w-full h-auto object-cover mb-3"/>
+        </div>
+      `,  },
+]
+
+const productVariationData = [
+    {
+      id: 1,
+      product_id: 1,
+      name: "Màu sắc",
+      position: 1,
+    },
+    {
+      id: 2,
+      product_id: 1,
+      name: "Kích cỡ",
+      position: 2,
+    },
+    {
+      id: 3,
+      product_id: 2,
+      name: "Kích cỡ",
+      position: 1,
+    }
+  ];
+
+  const productVariationOptionsData = [
+    // Product 1 - Colors
+    { id: 1, variation_id: 1, value: "Xanh" },
+    { id: 2, variation_id: 1, value: "Đỏ" },
+    { id: 3, variation_id: 1, value: "Vàng" },
+  
+    // Product 1 - Sizes
+    { id: 4, variation_id: 2, value: "M" },
+    { id: 5, variation_id: 2, value: "L" },
+  
+    // Product 2 - Sizes
+    { id: 6, variation_id: 3, value: "M" },
+    { id: 7, variation_id: 3, value: "L" },
+    { id: 8, variation_id: 3, value: "XL" },
+  ];
+
+  const productSkusData = [
+    // Product 1
+    { id: 1, product_id: 1, sku: "P1-XANH-M", quantity: 0 },
+    { id: 2, product_id: 1, sku: "P1-XANH-L", quantity: 8 },
+    { id: 3, product_id: 1, sku: "P1-DO-M", quantity: 5 },
+    { id: 4, product_id: 1, sku: "P1-DO-L", quantity: 2 },
+    { id: 5, product_id: 1, sku: "P1-VANG-M", quantity: 6 },
+    { id: 6, product_id: 1, sku: "P1-VANG-L", quantity: 4 },
+  
+    // Product 2
+    { id: 7, product_id: 2, sku: "P2-M", quantity: 20 },
+    { id: 8, product_id: 2, sku: "P2-L", quantity: 0 },
+    { id: 9, product_id: 2, sku: "P2-XL", quantity: 0 },
+  ];
+
+  const productSkuOptionsData = [
+    // Product 1 - Color + Size combinations
+    { id: 1, sku_id: 1, option_id: 1 }, // Xanh
+    { id: 2, sku_id: 1, option_id: 4 }, // M
+  
+    { id: 3, sku_id: 2, option_id: 1 }, // Xanh
+    { id: 4, sku_id: 2, option_id: 5 }, // L
+  
+    { id: 5, sku_id: 3, option_id: 2 }, // Đỏ
+    { id: 6, sku_id: 3, option_id: 4 }, // M
+  
+    { id: 7, sku_id: 4, option_id: 2 }, // Đỏ
+    { id: 8, sku_id: 4, option_id: 5 }, // L
+  
+    { id: 9, sku_id: 5, option_id: 3 }, // Vàng
+    { id: 10, sku_id: 5, option_id: 4 }, // M
+  
+    { id: 11, sku_id: 6, option_id: 3 }, // Vàng
+    { id: 12, sku_id: 6, option_id: 5 }, // L
+  
+    // Product 2
+    { id: 13, sku_id: 7, option_id: 6 }, // M
+    { id: 14, sku_id: 8, option_id: 7 }, // L
+    { id: 15, sku_id: 9, option_id: 8 }, // XL
+  ];
+
+  export { productsData, productVariationData, productVariationOptionsData, productSkusData, productSkuOptionsData };
